38a27fa4592ee5e911aa96d41ed0581262427449
ZabouG <adrian@alixir.fr>
2024-08-10 11:21:17 +0400

    refonte visuel

--- conf.js
+++ conf.js
@@ -1,6 +1,14 @@
-var VilleIcon = L.icon({
-    iconUrl: 'icon/batiment.png',
-    iconSize: [32, 32], // Taille de l'icône
-    iconAnchor: [16, 32], // Point d'ancrage de l'icône
-    popupAnchor: [0, -32] // Point d'ancrage du popup
+var CapitalIcon = L.icon({
+    iconUrl: 'icon/capitalPing.png',
+    iconSize: [15, 15], // Taille de l'icône
+    iconAnchor: [0, 10], // Point d'ancrage de l'icône
+    popupAnchor: [8, 0], // Point d'ancrage du popup
+    className: 'custom-icon' // Ajout d'une classe CSS personnalisée
+  });
+
+  var VilleIcon = L.icon({
+    iconUrl: 'icon/villePing.png',
+    iconSize: [15, 15 ], // Taille de l'icône
+    iconAnchor: [0, 10], // Point d'ancrage de l'icône
+    popupAnchor: [6, 0], // Point d'ancrage du popup
   });
