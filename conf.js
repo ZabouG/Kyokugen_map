de93a22701d621b2130d40ae8f646604aa016dc1
ZabouG <adrian@alixir.fr>
2024-08-10 11:26:55 +0400

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,14 +1,21 @@
 var CapitalIcon = L.icon({
     iconUrl: 'icon/capitalPing.png',
-    iconSize: [15, 15], // Taille de l'icône
-    iconAnchor: [0, 10], // Point d'ancrage de l'icône
-    popupAnchor: [8, 0], // Point d'ancrage du popup
+    iconSize: [30, 30], // Taille de l'icône
+    iconAnchor: [10, 20], // Point d'ancrage de l'icône
+    popupAnchor: [6, -15], // Point d'ancrage du popup
     className: 'custom-icon' // Ajout d'une classe CSS personnalisée
   });
 
   var VilleIcon = L.icon({
     iconUrl: 'icon/villePing.png',
-    iconSize: [15, 15 ], // Taille de l'icône
-    iconAnchor: [0, 10], // Point d'ancrage de l'icône
-    popupAnchor: [6, 0], // Point d'ancrage du popup
+    iconSize: [30, 30 ], // Taille de l'icône
+    iconAnchor: [10, 20], // Point d'ancrage de l'icône
+    popupAnchor: [6, -15], // Point d'ancrage du popup
+  });
+
+  var PrisonIcon = L.icon({
+    iconUrl: 'icon/prisonPing.png',
+    iconSize: [30, 30 ], // Taille de l'icône
+    iconAnchor: [10, 20], // Point d'ancrage de l'icône
+    popupAnchor: [6, -15], // Point d'ancrage du popup
   });
