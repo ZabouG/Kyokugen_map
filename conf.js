16aa3faaaeb7464ad359268bd140303a6cf41d2b
ZabouG <adrian@alixir.fr>
2024-08-10 21:58:09 +0400

    new icon
    Ajoute de nouvelle icon et de zone dangereuse

--- conf.js
+++ conf.js
@@ -19,3 +19,10 @@ var CapitalIcon = L.icon({
     iconAnchor: [10, 20], // Point d'ancrage de l'icône
     popupAnchor: [6, -15], // Point d'ancrage du popup
   });
+
+  var WarningIcon = L.icon({
+    iconUrl: 'icon/avertissement.png',
+    iconSize: [30, 30 ], // Taille de l'icône
+    iconAnchor: [10, 20], // Point d'ancrage de l'icône
+    popupAnchor: [6, -15], // Point d'ancrage du popup
+  });
