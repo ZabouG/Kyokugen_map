10629dd51751c1eecd66a3392c97609dffd544c2
ZabouG <adrian@alixir.fr>
2024-08-10 07:10:34 +0400

    Controle scroll + filtre des point

---
+++ conf.js
@@ -0,0 +1,6 @@
+var VilleIcon = L.icon({
+    iconUrl: 'icon/batiment.png',
+    iconSize: [32, 32], // Taille de l'icône
+    iconAnchor: [16, 32], // Point d'ancrage de l'icône
+    popupAnchor: [0, -32] // Point d'ancrage du popup
+  });
